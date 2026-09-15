c4678b35c732c0d186fb3e5605fcd1605ae233c1
Abjecs <alekseyi.berezhnyak@gmail.com>
2026-09-15 22:54:51 +0300

    Expose database API explicitly

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,3 @@
-from .connection import init_db, db_pool, get_pool
+from .connection import check_db, close_db, db_pool, get_pool, init_db
+
+__all__ = ["check_db", "close_db", "db_pool", "get_pool", "init_db"]
