7ee97465b1908b89c849663588aecbd4b412ca69
Abjecs <alekseyi.berezhnyak@gmail.com>
2026-09-16 12:07:30 +0300

    Remove legacy chatbot modules and keep trading stack

--- database/__init__.py
+++
@@ -1,3 +0,0 @@
-from .connection import check_db, close_db, db_pool, get_pool, init_db
-
-__all__ = ["check_db", "close_db", "db_pool", "get_pool", "init_db"]
--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault("TELEGRAM_TOKEN", "test-token")
-os.environ.setdefault("GIGACHAT_CREDENTIALS", "test-credentials")
-os.environ.setdefault("DATABASE_URL", "postgresql://test:test@localhost/test")
